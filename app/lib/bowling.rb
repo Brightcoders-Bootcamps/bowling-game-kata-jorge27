d795538be9c94200e729ba32a749c07d73f7a45d
jorge27 <ja.garciavega@outlook.com>
2020-06-19 19:19:46 -0500

    Delete getters, now, declare class propieties with attr_reader
    Also defined some methods as private methods, and modified some if's for
    show in one line

--- app/lib/bowling.rb
+++ app/lib/bowling.rb
@@ -2,6 +2,8 @@
 #  Bowling class capture and calcule the punctuation in game
 #
 class Bowling
+  attr_reader :rounds, :points, :turn
+
   def initialize
     # => poins contain array with puntuation in game
     @points = Array.new(10) { nil }
@@ -15,32 +17,29 @@ class Bowling
 
   def capture(primer_tiro, segundo_tiro)
     begin
-      primer_tiro, segundo_tiro, bowl = Integer(primer_tiro), Integer(segundo_tiro), primer_tiro + segundo_tiro
+      primer_tiro, segundo_tiro, bowl = Integer(primer_tiro), Integer(segundo_tiro), primer_tiro.to_i + segundo_tiro.to_i
     rescue
       return nil
     end
 
-    if primer_tiro.negative? || segundo_tiro.negative? || segundo_tiro > 10 || primer_tiro > 10
-      return nil
-    end
-    if (@turn!=10) && ((bowl).negative? || (bowl) > 10)
-      return nil
-    end
+    return nil if (primer_tiro.negative? || segundo_tiro.negative? || segundo_tiro > 10 || primer_tiro > 10)
+    return nil if (@turn!=10) && ((bowl).negative? || (bowl) > 10)
 
-    @rounds[@turn] = [primer_tiro, segundo_tiro]
+    @rounds[@turn] = [primer_tiro.to_i, segundo_tiro.to_i]
   end
 
   def calculate
     if @turn < 10
       @points[@turn] = @rounds[@turn].sum
     end
-    self.is_strike
-    self.is_spare
+    is_strike
+    is_spare
 
     @turn += 1
     @points
   end
 
+  private 
   def is_strike
     last, two_last, last_bowling = @turn - 1, @turn - 2, @rounds[@turn][0]
     if (two_last) >= 0
@@ -52,27 +51,15 @@ class Bowling
   end
 
   def is_spare
-    last_turn, shoot, last_shoot, points_last, points_this = @turn - 1, @rounds[@turn], @rounds[last_turn], @points[last_turn], @points[@turn]
-    if (last_turn) >= 0
+    last_turn, shoot, last_shoot, points_last, points_this = (@turn - 1), @rounds[@turn], @rounds[@turn-1], @points[@turn-1], @points[@turn]
+    if last_turn >= 0
       if (last_shoot.sum) == 10
         points_last += (last_shoot == [10, 0]) ? shoot.sum : shoot[0]
       end
       if @turn < 10
         points_this += points_last
       end
-      @points[last_turn], @points[@turn] = points_last, points_this
     end
+    @points[last_turn], @points[@turn] = points_last, points_this
   end
-
-  def turn
-    @turn
-  end
-
-  def rounds(num)
-    @rounds[num]
-  end
-  
-  @points
-  @rounds
-  @turn
 end
